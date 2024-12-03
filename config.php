8e277741ef2d39c1e42040730ebc60cfa7d591da
GewoonNiek <berkhofniek0@gmail.com>
2024-12-03 14:36:29 +0100

    Fixed config and register.php

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=
-    muziekPHP", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=muziekphp", "root", "");
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
--- register.php
+++ register.php
@@ -1,40 +1,42 @@
-<form?php include("config.php"); $con=$db->prepare("insert into user set username=?, password=?, email=?");
+<?php
+include("config.php");
 
-    if ($_POST) {
+$con = $db->prepare("insert into user set username=?, password=?, email=?");
+
+if ($_POST) {
     $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
     $password = md5(addslashes($_POST["password"]));
     $email = htmlspecialchars($_POST["email"], ENT_QUOTES);
 
     if (!$username || !$password || !$email) {
-    echo "All fields must be filled in...";
+        echo "All fields must be filled in...";
     } else {
-    $x = $con->execute(array($username, $password, $email));
+        $x = $con->execute(array($username, $password, $email));
 
-    if ($x) {
-    echo "Registration finished! U are being sent to the main page";
-    header("refresh:5; url=index.php");
+        if ($x) {
+            echo "Registration finished! U are being sent to the main page";
+            header("refresh:5; url=index.php");
+        }
     }
-    }
-    }
+}
 
-    ?>
+?>
 
-    <form method="post" action="">
-        <table>
-            <tr>
-                <td>Username:</td>
-                <td><input type="text" name="username"></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td><input type="submit" value="Submit"></td>
-            </tr>
-        </table>
-    </form>
+<form method="post" action="">
+    <table>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Submit"></td>
+        </tr>
+    </table>
