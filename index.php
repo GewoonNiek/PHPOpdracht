10e9a72d0eb762fd77e978da63dc537818c939a3
Cedric <Cedri@FUNNYMAN>
2025-01-08 20:50:13 +0100

    trying to add a image

--- index.php
+++ index.php
@@ -11,16 +11,17 @@ try {
     } else {
         foreach ($articles as $article) {
             ?>
-            <div style="border:1px solid #ddd; height:300px; width:500px; margin:5px; padding:10px; float:left;">
+            <div style="border:1px solid #ddd; height:200px; width:500px; margin:5px; padding:10px; float:left;">
                 <h2 style="border:1px solid #ddd; background:pink; font-size: 14px; font-family: Verdana;">
-                    <?php echo htmlspecialchars($article["name"], ENT_QUOTES, 'UTF-8'); ?>
+                    <?php echo htmlspecialchars($article["artikel_Name"] . " - ", ENT_QUOTES,'UTF-8'); ?>
+                    <?php echo htmlspecialchars("Stock: ". $article["amount"], ENT_QUOTES, 'UTF-8'); ?>
                 </h2>
-                <p>
-                    <?php echo nl2br(htmlspecialchars($article["description"], ENT_QUOTES, 'UTF-8')); ?>
-                </p>
-                <br>
-                <a onclick="return confirm('Weet u zeker dat u dit product wilt verwijderen?');" 
-                   href="deleteItem.php?id=<?php echo urlencode($article['id']); ?>">Verwijder item!</a>
+                <?php if (!empty($article["artikel_IMG"])) { ?>
+                    <img src="<?php echo htmlspecialchars($article["artikel_IMG"], ENT_QUOTES, 'UTF-8'); ?>" 
+                         alt="Image" style="max-width:100%; max-height:100px;">
+                <?php } else { ?>
+                    <p>No image available.</p>
+                <?php } ?>
             </div>
             <?php
         }
